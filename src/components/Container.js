9da3f5fb3c201a14fa9aabf33fcc9c9315d8eea5
LuisDHR <ldwhr01@gmail.com>
2021-09-28 11:32:36 -0500

    Arreglar detalles finales

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,5 +1,5 @@
 // Componente que muestra la barra de título, los 
-// Pokémon obtenidos, la página actual y una botonera 
+// pokémones obtenidos, la página actual y una botonera 
 // para cambiar ascendente y descendentemente el número de página.
 import { useState, useEffect } from 'react'
 import Titlebar from "./Titlebar/Titlebar"
@@ -9,14 +9,14 @@ import {getPokemonList} from '../services/pokemonService'
 
 const Container = () => {
     // Hooks de estado para almacenar/cambiar la página actual
-    // y la lista de Pókemon.
+    // y la lista de pokémones.
     const [page, setPage] = useState(0)
     const [pokemonList, setPokemonList] = useState([])
     // Hook de estado para mostrar/controlar el mensaje de 'Cargando...'.
     const [loading, setLoading] = useState(true)
     // Hooks de estado para controlar el estado de los botones.
-    const [btnSigActivo, setBtnSigActivo] = useState(false)
-    const [btnAntActivo, setBtnAntActivo] = useState(true)
+    const [btnNext, setBtnNext] = useState(false)
+    const [btnPrevious, setBtnPrevious] = useState(true)
 
     // Se obtienen los primeros 10 pokémon.
     useEffect( () => {
@@ -30,30 +30,30 @@ const Container = () => {
 
     // Función para pasar a la siguiente página.
     const siguiente = async () => {
-        setBtnSigActivo(true)
+        setBtnNext(true)
         setLoading(true)
         if (page < 14) {
             let actual = page + 1
             setPage(actual)
             setPokemonList( await getPokemonList(actual) )
             
-            if (actual < 14) setBtnSigActivo(false)
-            if (actual > 0) setBtnAntActivo(false)
+            if (actual < 14) setBtnNext(false)
+            if (actual > 0) setBtnPrevious(false)
         }
         setLoading(false)
     }
 
     // Función para pasar a la página anterior.
     const anterior = async () => {
-        setBtnAntActivo(true)
+        setBtnPrevious(true)
         setLoading(true)
         if (page > 0) {
             let actual = page - 1
             setPage(actual)
             setPokemonList( await getPokemonList(actual) )
 
-            if (actual > 0) setBtnAntActivo(false)
-            if (actual < 14) setBtnSigActivo(false)
+            if (actual > 0) setBtnPrevious(false)
+            if (actual < 14) setBtnNext(false)
         }
         setLoading(false)
     }
@@ -63,8 +63,8 @@ const Container = () => {
         <Titlebar />
         <div className="pagina">Página {page + 1}</div>
         <div className="btn">
-            <Button onClick={anterior} disabled={btnAntActivo} >Anterior</Button>
-            <Button onClick={siguiente} disabled={btnSigActivo}>Siguiente</Button>
+            <Button onClick={anterior} disabled={btnPrevious} >Anterior</Button>
+            <Button onClick={siguiente} disabled={btnNext}>Siguiente</Button>
         </div>
         {   loading
             ? <div className="cargando">Cargando...</div>
